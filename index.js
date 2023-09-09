b75cb839dcb13a9f935782b0016d7418ebe090bb
George Rabadi <96255554+GeorgeRabadi@users.noreply.github.com>
2023-09-09 02:29:05 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,108 @@
+const express = require('express')
+const app = express()
+app.use(express.json())
+app.use(express.static('dist'))
+
+var morgan = require('morgan')
+
+morgan.token('body', req => {
+  return JSON.stringify(req.body)
+})
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+
+const cors = require('cors')
+
+app.use(cors())
+
+
+
+let persons = [
+    { 
+      "id": 1,
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": 2,
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": 3,
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": 4,
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+]
+
+
+app.get('/api/persons', (request, response) => {
+    response.json(persons)
+  })
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(persons =>  persons.id === id)
+
+    if (person) 
+        response.json(person)
+    else 
+        response.status(404).end()
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+    
+    const id = Number(request.params.id)
+    persons = persons.filter(person => person.id !== id)
+    
+    response.status(204).end()
+})
+
+
+app.post('/api/persons', (request, response) => {
+    
+    const body = request.body
+  
+    if (!(body.name && body.number)) {
+      return response.status(400).json({ 
+        error: 'name or number missing' 
+      })
+    }
+
+    if(persons.find(person => person.name == body.name) != null)
+        return response.status(400).json({ 
+            error: 'name must be unique' 
+        })
+
+  
+    const person = {
+      "name": body.name,
+      "number": body.number ,
+      "id": Math.floor(Math.random() * 1000)
+    }
+  
+    persons = persons.concat(person)
+  
+    response.json(person)
+})
+
+app.get('/info', (request, response) => {
+
+    const date = new Date()
+    response.send('<div>Phonebook has info for ' + persons.length + ' people</div><br/><div> ' + date + ' </div>')
+
+
+    
+})
+
+
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
